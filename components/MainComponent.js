2c2267e5405226e5f25ce5fce0a895b5e169ae9f
Kai Wang <charles.wangkai@gmail.com>
2018-11-06 22:12:04 +0800

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator(
     {
@@ -24,6 +24,45 @@ const MenuNavigator = createStackNavigator(
     }
 );
 
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                title: 'Home',
+                drawerLabel: 'Home'
+            }
+        },
+        Menu: {
+            screen: MenuNavigator,
+            navigationOptions: {
+                title: 'Menu',
+                drawerLabel: 'Menu'
+            }
+        }
+    },
+    {
+        drawerBackgroundColor: '#D1C4E9'
+    }
+);
+
 class Main extends Component {
     render() {
         return (
@@ -36,7 +75,7 @@ class Main extends Component {
                             : Expo.Constants.statusBarHeight
                 }}
             >
-                <MenuNavigator />
+                <MainNavigator />
             </View>
         );
     }
