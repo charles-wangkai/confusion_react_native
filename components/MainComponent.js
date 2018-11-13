c90a0386e22f6f275a791d29f4502a86e8ae5186
Kai Wang <charles.wangkai@gmail.com>
2018-11-14 00:24:52 +0800

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import Reservation from './ReservationComponent';
 import {
     View,
     Platform,
@@ -144,6 +145,31 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: (
+                <Icon
+                    name="menu"
+                    size={24}
+                    color="white"
+                    onPress={() => navigation.toggleDrawer()}
+                />
+            )
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -225,6 +251,21 @@ const MainNavigator = createDrawerNavigator(
                         name="address-card"
                         type="font-awesome"
                         size={22}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                title: 'Reserve Table',
+                drawerLabel: 'Reserve Table',
+                drawerIcon: ({ tintColor }) => (
+                    <Icon
+                        name="cutlery"
+                        type="font-awesome"
+                        size={24}
                         color={tintColor}
                     />
                 )
