e6745d3a1aba67e324b7462d2b47019f4b53b5a6
Kai Wang <charles.wangkai@gmail.com>
2018-11-17 19:19:51 +0800

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 import {
     View,
     Platform,
@@ -170,6 +171,31 @@ const ReservationNavigator = createStackNavigator(
     }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: (
+                <Icon
+                    name="menu"
+                    size={24}
+                    color="white"
+                    onPress={() => navigation.toggleDrawer()}
+                />
+            )
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -251,6 +277,21 @@ const MainNavigator = createDrawerNavigator(
                         name="address-card"
                         type="font-awesome"
                         size={22}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                title: 'My Favorites',
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({ tintColor }) => (
+                    <Icon
+                        name="heart"
+                        type="font-awesome"
+                        size={24}
                         color={tintColor}
                     />
                 )
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.1.125:3001/';
+export const baseUrl = 'http://192.168.44.73:3001/';
